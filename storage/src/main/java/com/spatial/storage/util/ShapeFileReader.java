a00da71521efdf435c145af39bf0263c64f90c10
aquaruiz <gpetiaa@gmail.com>
2020-01-26 12:37:34 +0200

    implemented simple shape file reader

--- storage/src/main/java/com/spatial/storage/util/ShapeFileReader.java
+++ storage/src/main/java/com/spatial/storage/util/ShapeFileReader.java
@@ -1,50 +1,42 @@
 package com.spatial.storage.util;
 
-import com.vividsolutions.jts.geom.Geometry;
-import org.apache.commons.io.FileUtils;
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.swing.data.JFileDataStoreChooser;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class ShapeFileReader {
     public static void readShapeFile (MultipartFile multipart) throws IOException {
 
-        File shapefile = new File("java.io.tmpdir", multipart.getName());
+//        File file = JFileDataStoreChooser.showOpenFile("shp", null);
+        File file = new File("java.io.tmpdir", "ABW_roads.shp");
 
-        // Create the file using the touch method of the FileUtils class.
-        FileUtils.touch(shapefile);
 
-        // Write bytes from the multipart file to disk.
-        FileUtils.writeByteArrayToFile(shapefile, multipart.getBytes());
+        Map<String, Object> map = new HashMap<>();
+        map.put("url", file.toURI().toURL());
 
-        HashMap<String, URL> connect = new HashMap<String, URL>();
-        ArrayList<Geometry> result = new ArrayList<Geometry>();
+        DataStore dataStore = DataStoreFinder.getDataStore(map);
+        String typeName = dataStore.getTypeNames()[0];
 
-        connect.put("url", shapefile.toURI().toURL());
-        DataStore dataStore = DataStoreFinder.getDataStore(connect);
-        String[] typeNames = dataStore.getTypeNames();
-        String typeName = typeNames[0];
+        FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
 
-        System.out.println("Reading content " + typeName);
+        FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures();
 
-//        File convFile = new File(System.getProperty("java.io.tmpdir")+"/"+multipart.getName());
-//        multipart.transferTo(convFile);
-//
-//         TODO check if zip file
-//        URL shape = convFile.toURL();
-////        URL shape = new URL("jar:"+convFile+"!/ABW_roads.shp");
-//        Map<String, URL> params = new HashMap<>();
-//        params.put("url", shape);
-//        DataStore ds = DataStoreFinder.getDataStore(params);
-//        String name = ds.getTypeNames()[0];
-//        SimpleFeatureSource source = ds.getFeatureSource(name);
-//        System.out.println(name+" has "+source.getCount(Query.ALL)+" features");
+        DataStore store = DataStoreFinder.getDataStore(map);
+        SimpleFeatureSource featureSource = store.getFeatureSource(store.getTypeNames()[0]);
+
+        System.out.println( "Hello World!" );
+
 
     }
 }
